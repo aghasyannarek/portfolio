2c7e7409e6c7dff18dabf76b9d7fc0ad255bf4f2
aghasyannarekportfolio <aghasyan_narek@mail.ru>
2022-07-23 22:52:48 +0400

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,19 @@
 window.addEventListener("DOMContentLoaded", () => {
 	const scrollDown = document.querySelector(".scrollDown");
 	const main = document.querySelector("main");
+	const portfolio = document.querySelector(".portfolio");
+	const scrollUp = document.querySelector(".scrollup");
 
 	try {
 		scrollDown.addEventListener("click", (e) => {
+			main.scrollIntoView({
+				block: "start",
+				behavior: "smooth"
+			});
+		});
+	} catch{}
+	try {
+		portfolio.addEventListener("click", (e) => {
 			main.scrollIntoView({
 				block: "start",
 				behavior: "smooth"
